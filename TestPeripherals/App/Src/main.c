4070d2e4909ee5ea0d42f7e0fdc7b4b5cf4e580a
m-malgeri <mmalgeri@unal.edu.co>
2023-05-30 15:21:04 -0500

    Programa base para el test de un periferico

---
+++ TestPeripherals/App/Src/main.c
@@ -0,0 +1,41 @@
+/**
+ ******************************************************************************
+ * @file           : IntroGPIOxMain.c
+ * @author         : mmalgeri
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2023 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+#include <stm32f4xx.h>
+
+#include "BasicTimer.h"
+#include "ExtiDriver.h"
+#include "GPIOxDriver.h"
+#include "I2CDriver.h"
+#include "PwmDriver.h"
+#include "SysTickDriver.h"
+#include "USARTxDriver.h"
+
+
+int main (void)
+{
+	while (1)
+	{
+	}
+}
+
